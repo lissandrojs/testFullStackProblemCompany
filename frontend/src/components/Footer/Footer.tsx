677d003e9c5edede1f0bf06388ac298bb20e1412
Lissandro kenziano <lissandromr@hotmail.com>
2022-12-21 18:00:22 -0300

    feat : finished component footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,15 +1,27 @@
 import React from "react"
 import './style.css'
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { Link, Typography } from "@mui/material";
 
 const Footer =()=>{
     return(
         <div className="downbar">
-            <div>
-                <div>
-                    footer
-                </div>
+            <div className="firstBox">
+                <Typography variant="h5" component="h5">
+                    Ready for a growth partner?
+                </Typography >
+                <Typography variant="h5" component="h5">
+                    Yes, I am ready!
+                </Typography>
             </div>
-        
+            <div>
+                <Link href="https://github.com/lissandrojs" rel="noreferrer" target="_blank">
+                    <AiFillGithub style={{ width:"50px",height:"32px"}}/>
+                </Link>
+                <Link href="https://www.linkedin.com/in/lissandro-miranda-rocha/" rel="noreferrer" target="_blank">
+                    <AiFillLinkedin style={{ width:"50px",height:"32px"}}/>
+                </Link>
+            </div>
         </div>
         )
 }
