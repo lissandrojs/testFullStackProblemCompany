da44fd0148be2a4a4cb6b2b6d0355a2dec3e3fbf
Lissandro kenziano <lissandromr@hotmail.com>
2022-12-21 17:13:24 -0300

    feat : add logo in header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -4,7 +4,19 @@ import "./style.css"
 const Header =()=>{
     return(
         <div className="topbar">
-            header
+            <div>
+                <img style={{width:"65px"}}src="https://problemcompany.org/assets/logo/logo-only.svg"/>
+            </div>
+            <div className="userAction">
+                <ul>
+                    <li>
+                        user
+                    </li>
+                    <li>
+                        cart
+                    </li>
+                </ul>
+            </div>
         </div>
         )
 }
