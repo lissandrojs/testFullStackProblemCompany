97060271eaaca7fd93d42b7f75b2cc1750bdeab1
Lissandro kenziano <lissandromr@hotmail.com>
2022-12-22 17:11:19 -0300

    fix: remove auth router sellers

--- backend/src/server/sellersRouter.go
+++ backend/src/server/sellersRouter.go
@@ -8,7 +8,7 @@ import (
 
 func SellersRouter(router *gin.Engine) {
 	router.GET("/sellers", controllers.GetSellersController, middlewares.Auth())
-	router.POST("/sellers", controllers.PostSellersController, middlewares.Auth())
+	router.POST("/sellers", controllers.PostSellersController)
 	router.DELETE("/sellers/:id", controllers.DeleteSellersController, middlewares.Auth())
 	router.PUT("/sellers/:id", controllers.UpdateSellersController, middlewares.Auth())
 	router.POST("/sellers/login", controllers.PostLoginSellerController)
