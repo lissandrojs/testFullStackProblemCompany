6d471cd508efd75a9d1666b7f48044c06c04c719
Lissandro kenziano <lissandromr@hotmail.com>
2022-12-22 10:30:48 -0300

    fix : error cors

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	config "github.com/lissandrojs/testFullStackProblemCompany/backend/src/schema"
 	routes "github.com/lissandrojs/testFullStackProblemCompany/backend/src/server"
@@ -9,6 +10,17 @@ import (
 func main() {
 
 	router := gin.New()
+
+	router.Use(cors.New(cors.Config{
+		AllowMethods:     []string{"GET", "POST", "OPTIONS", "PUT"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "User-Agent", "Referrer", "Host", "Token"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowAllOrigins:  false,
+		AllowOriginFunc:  func(origin string) bool { return true },
+		MaxAge:           86400,
+	}))
+
 	routes.UsersRouter((router))
 	routes.ProductsRouter((router))
 	routes.SellersRouter((router))
